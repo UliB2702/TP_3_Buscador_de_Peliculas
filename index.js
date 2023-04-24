3dd2d5e99e4dc2306093c9afd5100afd771f62ed
ULISES ANIBAL BAAMONDE <45687573@ort>
2023-04-24 15:59:52 -0300

    Ya casi terminado

--- index.js
+++ index.js
@@ -76,30 +76,34 @@ nos permite realizar operaciones como cliente HTTP, basado en el modelo de prome
 
 //Si hacemos la misma consulta que hicimos con fetch pero con axios, ser vería así:
 
+let Buscar = event =>{
+event.preventDefault();
 console.log("Hago consulta - axios");
 (async () => {
     console.log("Hago async consulta - axios")
+    let titulo = document.getElementById("nombre2").value
+    let tipo = document.getElementById("Tipo").value
+    let Año = document.getElementById("año").value
+    console.log(tipo)
     await axios({
         method: 'get',
-        url: 'https://mercados.ambito.com//home/general',
+        url: 'https://www.omdbapi.com/?apikey=2f0f3be' + "&s=" + titulo + "&type=" + tipo + "&y=" + Año,
     })
+
     .then(res => {
-        console.log("res", res)
-        valores = document.getElementById("valores");
-        res.data.forEach(actual => {
-            if (actual.compra && actual.venta) {
-                dolar = document.createElement("li")
-                dolar.innerHTML = `Nombre: ${actual.nombre} - compra: ${actual.compra} - venta: ${actual.venta}`
-                valores.appendChild(dolar)    
-            }            
+        console.log("res", res.data.Search)
+        res.data.Search.forEach(actual=>{
+        pelicula=document.createElement("li") 
+        pelicula.innerHTML=`Nombre:${actual.Title} - Año:${actual.Year} - Tipo:${actual.Type}` 
+        valores.appendChild(pelicula)
         })
     })
-    .catch(err => console.error("error", err))
+    .catch(err => console.error("error", err)) 
     console.log("Fin async consulta - axios")
 })();
 console.log("Fin consulta - axios")
 
-
+}
 
 
 
